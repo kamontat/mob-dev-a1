ff31d43d7ae8d2170fe18a627a15954b4f36ea54
Kamontat Chantrachirathumrong <kamontat_c@hotmail.com>
2018-01-18 10:19:08 +0700

    Initial Commit

---
+++ SayWhatILike/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SayWhatILike
+//
+//  Created by kamontat chantrachirathumrong on 18/1/18.
+//  Copyright © 2018 kamontat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
