ea876c0a945d5589a88c9b8af9af81516028b724
Kamontat Chantrachirathumrong <kamontat_c@hotmail.com>
2018-01-18 11:26:17 +0700

    Complete Assignment {1} - {Class-2}

--- SayWhatILike/ViewController.swift
+++ SayWhatILike/ViewController.swift
@@ -9,17 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var outputField: UILabel!
+    @IBOutlet weak var inputTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        outputField.text = "default_label".localized
+    }
+    
+    @IBAction func sayWord(_ sender: UIButton) {
+        if let text = inputTextField.text {
+            if text == "" {
+                outputField.text = "default_label".localized
+            } else {
+                outputField.text = "\("iup".localized) is \(text.lowercased())";
+            }
+        } else {
+            outputField.text = "default_label".localized
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
+extension String {
+    var localized: String {
+        return NSLocalizedString(self, tableName: nil, bundle: Bundle.main, value: "", comment: "")
+    }
+}
